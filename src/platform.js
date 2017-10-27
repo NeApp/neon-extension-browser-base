54264e1a591250a9c820962942baf541de736630
Dean Gardiner <me@dgardiner.net>
2017-10-27 16:34:39 +1300

    Added the `Platform.version` property, and adjusted the naming of platforms and platform types

--- src/platform.js
+++ src/platform.js
@@ -1,29 +1,83 @@
+import Bowser from 'bowser';
+import Merge from 'lodash-es/merge';
+import ToNumber from 'lodash-es/toNumber';
+
 import {NotImplementedError} from 'neon-extension-framework/core/exceptions';
+import {isDefined} from 'neon-extension-framework/core/helpers';
 
 import {Base} from './base';
 
 
+const VersionRegex = /^(\d+)(?:\.(\d+))?/g;
+
 export const Platforms = {
-    Chrome: 'chrome',
-    Edge: 'edge',
-    Firefox: 'firefox',
-    Opera: 'opera'
+    Chrome: 'Chrome',
+    Edge: 'Edge',
+    Firefox: 'Firefox',
+    Opera: 'Opera'
 };
 
 export const PlatformTypes = {
-    WebExtensions: 'webextensions'
+    WebExtension: 'WebExtension'
 };
 
+export class Version {
+    constructor(name, options) {
+        options = Merge({
+            major: 0,
+            minor: 0
+        }, options || {});
+
+        this.name = name;
+
+        this.major = options.major;
+        this.minor = options.minor;
+    }
+
+    static parse(version, options) {
+        if(!isDefined(version)) {
+            return new Version(null);
+        }
+
+        // Match version against pattern
+        let match = VersionRegex.exec(version);
+
+        if(!isDefined(match)) {
+            return new Version(null);
+        }
+
+        // Create version
+        return new Version(version, {
+            major: ToNumber(match[1] || 0),
+            minor: ToNumber(match[2] || 0),
+
+            // Override with provided values
+            ...options
+        });
+    }
+}
+
 export class Platform extends Base {
+    constructor() {
+        super();
+
+        // Parse version
+        this._version = Version.parse(Bowser.version);
+    }
+
     static get supported() {
         return true;
+    }
+
+    get name() {
+        throw new NotImplementedError();
     }
 
     get type() {
         throw new NotImplementedError();
     }
 
-    get name() {
-        throw new NotImplementedError();
+    get version() {
+        return this._version;
     }
 }
