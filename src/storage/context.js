1156e383d604a31f40afd9175536e1d9b6f9d788
Dean Gardiner <me@dgardiner.net>
2017-12-14 14:04:27 +1300

    Updated the `StorageContext` class to support child contexts

--- src/storage/context.js
+++ src/storage/context.js
@@ -13,10 +13,6 @@ export class StorageContext extends Base {
         if(IsNil(this._name)) {
             throw new Error('Invalid value provided for the "name" parameter');
         }
-
-        if(this._name.indexOf(':') !== -1) {
-            throw new Error('Context names can\'t contain ":" characters');
-        }
     }
 
     static get supported() {
@@ -27,34 +23,42 @@ export class StorageContext extends Base {
         return this._name;
     }
 
+    context(name) {
+        return new StorageContext(this._storage, this.key(name));
+    }
+
+    key(key) {
+        return this._name + ':' + encodeURIComponent(key);
+    }
+
     remove(key) {
-        return this._storage.remove(this._buildKey(key));
+        return this._storage.remove(this.key(key));
     }
 
     // region Get
 
     get(key) {
-        return this._storage.get(this._buildKey(key));
+        return this._storage.get(this.key(key));
     }
 
     getBoolean(key) {
-        return this._storage.getBoolean(this._buildKey(key));
+        return this._storage.getBoolean(this.key(key));
     }
 
     getFloat(key) {
-        return this._storage.getFloat(this._buildKey(key));
+        return this._storage.getFloat(this.key(key));
     }
 
     getInteger(key) {
-        return this._storage.getInteger(this._buildKey(key));
+        return this._storage.getInteger(this.key(key));
     }
 
     getObject(key) {
-        return this._storage.getObject(this._buildKey(key));
+        return this._storage.getObject(this.key(key));
     }
 
     getString(key) {
-        return this._storage.getString(this._buildKey(key));
+        return this._storage.getString(this.key(key));
     }
 
     // endregion
@@ -62,35 +66,27 @@ export class StorageContext extends Base {
     // region Put
 
     put(key, value) {
-        return this._storage.put(this._buildKey(key), value);
+        return this._storage.put(this.key(key), value);
     }
 
     putBoolean(key, value) {
-        return this._storage.putBoolean(this._buildKey(key), value);
+        return this._storage.putBoolean(this.key(key), value);
     }
 
     putFloat(key, value) {
-        return this._storage.putFloat(this._buildKey(key), value);
+        return this._storage.putFloat(this.key(key), value);
     }
 
     putInteger(key, value) {
-        return this._storage.putInteger(this._buildKey(key), value);
+        return this._storage.putInteger(this.key(key), value);
     }
 
     putObject(key, value) {
-        return this._storage.putObject(this._buildKey(key), value);
+        return this._storage.putObject(this.key(key), value);
     }
 
     putString(key, value) {
-        return this._storage.putString(this._buildKey(key), value);
-    }
-
-    // endregion
-
-    // region Private methods
-
-    _buildKey(key) {
-        return this._name + ':' + key;
+        return this._storage.putString(this.key(key), value);
     }
 
     // endregion
